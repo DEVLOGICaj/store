a33c55d9b7a05fc13bef10ccbd5219b866ef3edd
DEVLOGICaj <abdalraheemjniad@gmail.com>
2026-07-28 01:16:46 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,7 @@
+const products = [
+  { name: "mouse", price: "12.00",image:"" },
+  { name: "keyboard", price: "15.00",image:"" },
+  { name: "screen", price: "35.00",image:"" },
+  { name: "laptop", price: "500.00",image:"" },
+  { name: "mouse242", price: "14.50",image:"" },
+];
