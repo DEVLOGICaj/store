56e12bc25a1f60c6e08ccae4bb98732121fe6787
DEVLOGICaj <abdalraheemjniad@gmail.com>
2026-07-28 03:53:12 +0300

    Update products.js

--- products.js
+++ products.js
@@ -1,7 +1,3 @@
 const products = [
-  { name: "mouse", price: "12.00",image:"" },
-  { name: "keyboard", price: "15.00",image:"" },
-  { name: "screen", price: "35.00",image:"" },
-  { name: "laptop", price: "500.00",image:"" },
-  { name: "mouse242", price: "14.50",image:"" },
+    { name: "منتج تجريبي", price: "10000 ليرة", image: "3.png" }
 ];
