5ebcb1b2dfa7c601c1ef0aa111abd138f7627e59
DEVLOGICaj <abdalraheemjniad@gmail.com>
2026-07-29 14:57:46 +0300

    Update products.js

--- products.js
+++ products.js
@@ -2,4 +2,5 @@ const products = [
 {name: "laptop HP",price: "125.00 $",image: "lap.jpg"},
 {name: "iphone17",price: "900.00 $",image: "17.jpg"},
 {name: "iphone17 promax",price: "1220.00 $",image: "pro.jpg"},
+{name: "tap2026",price: "134.00 $",image: ""},
 ];
